389e360600a13aca43a503f2d6d64d3c0fcc90b0
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2016-04-29 11:06:01 +0200

    ls_tags can now print multiple file

--- ls_tags/main.c
+++ ls_tags/main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <locale.h>
 
 #define _HEUR_SIZE 1024
 
@@ -11,33 +13,42 @@ static void usage(const char *imagefile)
     printf("usage: %s filename\n", imagefile);
 }
 
+static void ls_tags(const char *filename)
+{
+    char *buf = malloc(sizeof*buf * _HEUR_SIZE);
+    ssize_t r, s;
+
+    r = getxattr(filename, "tags", buf, _HEUR_SIZE);
+    if (r < 0) {
+        fprintf(stderr, "%s: erreur: %s\n", filename, strerror(errno));
+        free(buf);
+        return;
+    }
+
+    if (r > _HEUR_SIZE) {
+        buf = realloc(buf, r+1);
+        s = getxattr(filename, "tags", buf, r+1);
+        if (s < 0) {
+            perror(filename);
+            free(buf);
+            return;
+        }
+    }
+    buf[r] = 0;
+    printf("%s: %s\n", filename, buf);
+    free(buf);
+}
+
 int main(int argc, char *const argv[])
 {
     if (argc < 2) {
         usage(argv[0]);
         exit(EXIT_FAILURE);
     }
+    setlocale(LC_ALL, "");
+    for (int i = 1; i < argc; ++i)
+        ls_tags(argv[i]);
 
-    char *filename = argv[1];
-    char *buf = malloc(sizeof*buf * _HEUR_SIZE);
-    ssize_t r, s;
-
-    r = getxattr(filename, "tags", buf, _HEUR_SIZE);
-    if (r < 0) {
-        perror("getxattr");
-        exit(EXIT_FAILURE);
-    }
-
-    if (r > _HEUR_SIZE) {
-        buf = realloc(buf, r+1);
-        s = getxattr(argv[1], "tags", buf, r+1);
-        if (s < 0) {
-            perror("getxattr");
-            exit(EXIT_FAILURE);
-        }
-    } 
-    buf[r] = 0;
-    printf("%s: %s\n", filename, buf);
     return EXIT_SUCCESS;
 }
 
